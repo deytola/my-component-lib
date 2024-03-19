7e3c1aab44af2dde448452b30ffe89666131d53c
adetola <adebolaadetola65@gmail.com>
2024-03-19 08:28:21 -0600

    chore: update button component

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 const dts = require('rollup-plugin-dts');
+import postcss from 'rollup-plugin-postcss'
 
 const packageJson = require('./package.json');
 
@@ -25,12 +26,13 @@ export default [
             resolve(),
             commonjs(),
             typescript({tsconfig: './tsconfig.json'}),
-            // postcss(),
+            postcss(),
         ]
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts.default()],
+        external: [/\.css$/]
     }
 ]
